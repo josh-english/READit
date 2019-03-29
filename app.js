c4f5cabbee5d1aac6f196e4781ad4ff22939ffbd
Josh <joshua.c.english@gmail.com>
2019-03-29 12:39:26 -0400

    basic landing page with post page styling

--- app.js
+++ app.js
@@ -7,20 +7,28 @@ var app = express();
 app.set('view engine', "ejs");
 var mongoDB = 'mongodb://127.0.0.1/readit-db';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/assets"));
 
 var seed = require("./seed");
 seed();
 
 app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.get("/posts", function(req, res){
 	Post.find({}, function(err, posts){
 		if(err) {
 			console.log(err);
 		}
 		else {
-			res.render("index", {posts: posts});
+			res.render("posts/index", {posts: posts});
 		}
 	});
 });
+
+
 
 app.listen(port, function(){
 	console.log("Express Server is running!");
