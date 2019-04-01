c802a33d0a75c49c49189fda42b94c15e2eeb657
Josh <joshua.c.english@gmail.com>
2019-04-01 09:47:14 -0400

    added edit routes for posts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
 var Post = require("./models/post");
 var port = 3000;
 
@@ -9,6 +10,7 @@ app.set('view engine', "ejs");
 var mongoDB = 'mongodb://127.0.0.1/readit-db';
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
 
 var seed = require("./seed");
@@ -57,6 +59,31 @@ app.get("/posts/:id", function(req, res){
 		}
 		else {
 			res.render("posts/show", {post: post});
+		}
+	});
+});
+
+// edit route
+app.get("/posts/:id/edit", function(req, res){
+	Post.findById(req.params.id, function(err, post){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.render("posts/edit", {post: post});
+		}
+	});
+});
+
+// update route
+app.put("/posts/:id", function(req, res){
+	var post = {title: req.body.title};
+	Post.findByIdAndUpdate(req.params.id, post, function(err, updatedPost){
+		if(err){
+			console.log(err);
+		}
+		else {
+			res.redirect("/posts/" + req.params.id);
 		}
 	});
 });
